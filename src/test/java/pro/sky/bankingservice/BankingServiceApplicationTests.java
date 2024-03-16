4d0babe7e2f34841260ee6bd41c5cd391066cff3
Anton <Soloviev.gvardiya@gmail.com>
2024-03-16 11:07:38 +0300

    Initial commit

---
+++ src/test/java/pro/sky/bankingservice/BankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.bankingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
